8cc34d581a97dced761b14362522b20a08daeb46
liusibei <59981168+liusibei@users.noreply.github.com>
2020-02-27 21:18:21 +0800

    2. 西瓜数据集保存及读取
    添加编号列，并将数据集写入到machine_learning.csv文件，使用pandas读取验证文件是否有效(无错即可)。
    添加一条记录，青绿 硬挺 浊响 稍糊 平坦 硬滑 0.666 0.111 好
    再使用普通文件读取将数据集读取出来，列名读取到columns，数据(带编号)读取到datalist
    在所有数据中过滤出色泽='浅白'的数据
    在所有数据中过滤出密度大于0.5的数据

--- Lesson6_Assignment_python/work2.py
+++ Lesson6_Assignment_python/work2.py
@@ -29,14 +29,12 @@ with open('D:\machine_learning.csv','a') as f:
     for i in dataset2[1:]:
         i = f'\n{dataset2.index(i)}'+','+i
         f.write(i)
-print('csv file is generated!')
 
 # 向csv文件中加入一条新的数据（数据已给出）
 inser_data = '18 青绿 硬挺 浊响 稍糊 平坦 硬滑 0.666 0.111 是'
 inser_data1 = '\n'+inser_data.replace(' ',',')+'\n'
 with open(file,'a') as f:
     f.write(inser_data1)
-print('csv file is generated!')
 
 # 查看全体数据
 df = pd.read_csv(file,encoding='gbk')
@@ -55,8 +53,16 @@ with open(file,'r') as f:
 # 验证数据信息是否相符
 print(columns==['编号', '色泽', '根蒂', '敲声', '纹理', '脐部', '触感', '密度', '含糖率', '好瓜'])
 print(datalist[-1]==['18', '青绿', '硬挺', '浊响', '稍糊', '平坦', '硬滑', '0.666', '0.111', '是'])
-        
-    
+
+#将过滤的数据保存
+def func_filter(x):
+    for i in x:
+        if i==None:
+            pass
+        else:
+            with open(file,'a') as f:
+                f.write(i) 
+                
 # 在所有数据中过滤出色泽='浅白'的数据
 with open(file,'a') as f:
     f.write("\n")
@@ -65,26 +71,14 @@ def func(x):
     return s
 QB = list(filter(lambda x:'浅白'in x,datalist))
 QB1 = list(map(func,QB))
-for i in QB1:
-    if i==None:
-        pass
-    else:
-        with open(file,'a') as f:
-            f.write(i)
-print('csv file is generated!')
+func_filter(QB1)
 
 # 在所有数据中过滤出密度大于0.5的数据
 with open(file,'a') as f:
     f.write("\n")
 MD = list(filter(lambda x:False if x[7]=='密度' else float(x[7])>0.5,datalist))
 MD1 = list(map(func,MD))
-for i in MD1:
-    if i==None:
-        pass
-    else:
-        with open(file,'a') as f:
-            f.write(i)
-print('csv file is generated!')
+func_filter(MD1)
 
 # 查看全体数据
 df = pd.read_csv(file,encoding='gbk')
